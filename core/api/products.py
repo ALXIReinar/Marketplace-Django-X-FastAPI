e20b8f5a1eed175e68cf6e78b198671fdd0cb9c7
ALXIReinar <rodionova20047.a0@gmail.com>
2025-05-28 20:07:03 +0300

    Интеграция Селери с РеббитМКЬЮ в лице брокера сообщений, результаты селери-бэкенда хранятся в редисе. Частичная докеризация

--- core/api/products.py
+++ core/api/products.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter, Request
 
+from core.bg_tasks.celery_processing import lvl1_render
+from core.config_dir.debug_logger import log_debug
+from core.config_dir.logger import log_event
 from core.data.postgre import PgSqlDep
 from core.config_dir.base_dependencies import PagenDep
-from core.utils.anything import Tags
+from core.utils.anything import Tags, Events
 
 router = APIRouter(prefix='/api/products', tags=[Tags.products])
 
@@ -19,3 +22,24 @@ async def preview_products(pagen: PagenDep, request: Request, db: PgSqlDep):
             'counters': [
                 {'favorite': records.favorite}, {'ordered_items': records.ordered_items}]
             }
+
+
+
+@router.get('/{prd_id}_{seller_id}')
+async def get_product_card(
+        request: Request, db: PgSqlDep,
+        prd_id: int,
+        seller_id: int,
+        in_front_cache: bool = False
+):
+
+
+    product_inst_part = 'cached'
+    if not in_front_cache:
+        log_event(Events.bg_product_stage_.format('0' + ' | NO cached'), request, level='WARNING')
+        product_inst_part = await db.extended_product.instant_data_product_heavy(prd_id)
+    else:    log_event(Events.bg_product_stage_.format('0' + ' | cached!'), request)
+    task = lvl1_render.apply_async(args=[prd_id, in_front_cache])
+    return {'task-bg_lvl_1': task.id, 'product_info': product_inst_part}
+
+
